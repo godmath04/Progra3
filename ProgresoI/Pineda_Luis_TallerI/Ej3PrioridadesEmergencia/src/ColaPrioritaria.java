1d2926881a80177505ec33f16b8d04b43efb3bfc
Luis Elian Pineda Revelo <luis.pineda.revelo@udla.edu.ec>
2024-10-27 18:21:01 -0500

    Ejercicio 2 finalizado

---
+++ ProgresoI/Pineda_Luis_TallerI/Ej3PrioridadesEmergencia/src/ColaPrioritaria.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class ColaPrioritaria {
+    public Nodo frente;
+
+    public ColaPrioritaria(){
+        this.frente = null;
+    }
+    //Verificar si esta vacia
+    public boolean estaVacia(){
+        return frente == null;
+    }
+
+    //Mostrar cola
+    public void mostrarCola(JTextArea textArea){
+        if ()
+    }
+
+}
